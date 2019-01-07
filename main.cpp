6aaa79b50743dc1b124e4e2053e51537b94a10c2
Qiuk17 <32919465+Qiuk17@users.noreply.github.com>
2019-01-07 23:44:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 // Copyright qiuk17@outlook.com
 #include <iostream>
 
-int main() {
+int main()
+{
   int a, b;
+  
   std::cin >> a >> b;
   std::cout << a + b << std::endl;
   return 0;
+  
+  
 }
